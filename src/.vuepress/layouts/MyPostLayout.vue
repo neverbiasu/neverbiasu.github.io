75d513e659a87c2654bdff31ee970b0fa7bcc7ef
neverbiasu <1751162157@qq.com>
2024-10-03 14:59:49 +0800

    Update blog home page layout

---
+++ src/.vuepress/layouts/MyPostLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import CommonWrapper from "@theme-hope/components/CommonWrapper";
+import NormalPage from "@theme-hope/components/NormalPage";
+import SkipLink from "@theme-hope/components/SkipLink";
+import { FadeSlideY } from "@theme-hope/components/transitions/index";
+// 說明參考
+// https://theme-hope.vuejs.press/zh/guide/customize/layout.html
+// https://theme-hope.vuejs.press/zh/guide/advanced/replace.html
+// 範例參考
+// https://github.com/vuepress-theme-hope/vuepress-theme-hope/blob/main/docs/theme/src/.vuepress/layouts/SlotDemo.vue
+</script>
+<template>
+  <SkipLink />
+  <CommonWrapper>
+    <FadeSlideY>
+      <NormalPage>
+        <template #contentAfter>
+          <div class="theme-hope-content">
+            <Share colorful services="email,facebook,line,linkedin,messenger,qrcode,reddit,telegram,twitter" />
+          </div>
+        </template>
+      </NormalPage>
+    </FadeSlideY>
+  </CommonWrapper>
+</template>
