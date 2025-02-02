090f966b16c598e07abcaac7a8b47a1f493e66b9
neverbiasu <1751162157@qq.com>
2025-02-03 02:44:28 +0800

    feat: Dynamic generate navbar children

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -1,4 +1,52 @@
 import { navbar } from "vuepress-theme-hope";
+import { readdirSync, statSync } from "fs";
+import { join } from "path";
+
+function getMarkdownFiles(directory: string): string[] {
+  return readdirSync(directory)
+    .filter((file) => file.endsWith(".md"))
+    .map((file) => file.replace(".md", ""))
+    .sort();
+}
+
+function getSubdirectories(directory: string): string[] {
+  return readdirSync(directory).filter((subdir) =>
+    statSync(join(directory, subdir)).isDirectory()
+  );
+}
+
+function getChildren(
+  directory: string
+): { text: string; icon: string; link: string }[] {
+  return getMarkdownFiles(directory).map((file) => ({
+    text: file,
+    icon: "pen-to-square",
+    link: file,
+  }));
+}
+
+function getAllChildren(
+  basePath: string,
+  subdirectories: string[]
+): {
+  text: string;
+  icon: string;
+  prefix: string;
+  children: { text: string; icon: string; link: string }[];
+}[] {
+  return subdirectories.map((subdir) => ({
+    text: subdir,
+    icon: "pen-to-square",
+    prefix: `${subdir}/`,
+    children: getChildren(join(basePath, subdir)),
+  }));
+}
+
+const basePath = join(__dirname, "@/");
+
+const subdirectories = getSubdirectories(basePath);
+
+const allChildren = getAllChildren(basePath, subdirectories);
 
 export const zhNavbar = navbar([
   "/zh/",
@@ -7,36 +55,6 @@ export const zhNavbar = navbar([
     text: "博客",
     icon: "pen-to-square",
     prefix: "/zh/posts/",
-    children: [
-      {
-        text: "人工智能实现",
-        icon: "pen-to-square",
-        prefix: "ai-impls/",
-        children: [
-          { text: "YOLOv9代码复现", icon: "pen-to-square", link: "yolov9" },
-        ],
-      },
-      {
-        text: "前端开发",
-        icon: "pen-to-square",
-        prefix: "web/",
-        children: [
-          { text: "Vue3快速开始", icon: "pen-to-square", link: "vue-1" },
-        ],
-      },
-      {
-        text: "AI周报",
-        icon: "pen-to-square",
-        prefix: "weekly/",
-        children: [
-          { text: "001", icon: "pen-to-square", link: "001" },
-          { text: "002", icon: "pen-to-square", link: "002" },
-          { text: "003", icon: "pen-to-square", link: "003" },
-          { text: "004", icon: "pen-to-square", link: "004" },
-          { text: "005", icon: "pen-to-square", link: "005" },
-          { text: "X01", icon: "pen-to-square", link: "X01" },
-        ],
-      },
-    ],
+    children: allChildren,
   },
 ]);
